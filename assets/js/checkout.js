84e6d55cdbc282c81dcbb78c3cd2b778d4fd800d
casatainer <business.casatainer@gmail.com>
2024-06-05 08:42:57 +0200

    Added click to remove.

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,48 +1,48 @@
-function reload(){
+function reload() {
   let names = {
     basic: "Básico",
     normal: "Normal",
     advanced: "Avanzado"
   }
   let prices = {
-    basic:5000,
-    normal:10000,
-    advanced:15000
+    basic: 5000,
+    normal: 10000,
+    advanced: 15000
   }
   let cart = CartManager.getData();
 
-  let html_cart="";
-  for (let type of ["basic","normal","advanced"]) {
+  let html_cart = "";
+  for (let type of ["basic", "normal", "advanced"]) {
     if (cart[type]) {
       let name = names[type]
-      html_cart+=`<li class="list-group-item d-flex justify-content-between lh-sm">
+      html_cart += `<li class="list-group-item d-flex justify-content-between lh-sm">
       <div>
         <h6 class="my-0">Casatainer · ${name} <small>x${cart[type]}</small></h6>
-        <small class="text-body-secondary">${prices[type].toLocaleString("es-es")} €</small>
+        <small class="text-body-secondary" class="btnrem-${type}">${prices[type].toLocaleString("es-es")} €</small>
       </div>
-      <span class="text-body-secondary">${(prices[type]*cart[type]).toLocaleString("es-es")} €</span>
+      <span class="text-body-secondary">${(prices[type] * cart[type]).toLocaleString("es-es")} €</span>
     </li>`
     }
   }
   let reduction = 0;
-  let total_nopromo = (prices.basic*cart["basic"]+prices.normal*cart["normal"]+prices.advanced*cart["advanced"]);
+  let total_nopromo = (prices.basic * cart["basic"] + prices.normal * cart["normal"] + prices.advanced * cart["advanced"]);
   let params = new URLSearchParams(window.location.search);
   let discount = 0;
   let promo = params.get("promocode");
 
   if (promo) {
-    if (promo==="helloworld") {
-      discount=20;
+    if (promo === "helloworld") {
+      discount = 20;
     }
-    if (promo==="hacker") {
-      discount=100;
+    if (promo === "hacker") {
+      discount = 100;
     }
   }
 
-  reduction = total_nopromo*(discount/100)
+  reduction = total_nopromo * (discount / 100)
 
-  if(discount>0) {
-    html_cart+=`<li class="list-group-item d-flex justify-content-between bg-body-tertiary">
+  if (discount > 0) {
+    html_cart += `<li class="list-group-item d-flex justify-content-between bg-body-tertiary">
     <div class="text-success">
       <h6 class="my-0">Código promocional</h6>
       <small>${promo} · ${discount}%</small>
@@ -51,17 +51,17 @@ function reload(){
   </li>`
   }
 
-  let total=total_nopromo-reduction;
+  let total = total_nopromo - reduction;
 
-  html_cart+=`
+  html_cart += `
   <li class="list-group-item d-flex justify-content-between">
     <span>Total (EUR)</span>
     <strong>${total.toLocaleString("es-es")} €</strong>
   </li>`
 
-  document.querySelector("#count-cart").innerHTML=cart["basic"]+cart["normal"]+cart["advanced"]
+  document.querySelector("#count-cart").innerHTML = cart["basic"] + cart["normal"] + cart["advanced"]
 
-  document.querySelector("#scart").innerHTML=html_cart;
+  document.querySelector("#scart").innerHTML = html_cart;
 }
 
 (() => {
@@ -83,11 +83,26 @@ function reload(){
   });
 
 
-reload();
-  document.querySelector("#btn-redeem").addEventListener("click",e=>{
+  reload();
+  document.querySelector("#btn-redeem").addEventListener("click", e => {
     e.preventDefault();
     const params = new URLSearchParams();
-    params.set("promocode",document.querySelector("#redeem-code").value);
-    window.location.replace(window.location.origin+window.location.pathname+"?"+params.toString());
+    params.set("promocode", document.querySelector("#redeem-code").value);
+    window.location.replace(window.location.origin + window.location.pathname + "?" + params.toString());
+  })
+  document.querySelector("#btnrem-basic").addEventListener("click", e => {
+    e.preventDefault();
+    CartManager.addCartItem("basic", -1);
+    reload();
+  })
+  document.querySelector("#btnrem-normal").addEventListener("click", e => {
+    e.preventDefault();
+    CartManager.addCartItem("normal", -1);
+    reload();
+  })
+  document.querySelector("#btnrem-advanced").addEventListener("click", e => {
+    e.preventDefault();
+    CartManager.addCartItem("advanced", -1);
+    reload();
   })
 })()
